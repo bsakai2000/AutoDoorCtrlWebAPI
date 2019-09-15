c9ef3cb39f648f81f2b857c069c79f1620142d45
Brandon Sakai <brandon.sakai@gmail.com>
2019-09-15 14:58:14 +0000

    Updated API to use mysql, with bcrypt for password hashing

--- Server.js
+++ Server.js
@@ -1,8 +1,9 @@
 //Initiallising node modules
 var express = require("express");
 var bodyParser = require("body-parser");
-var sql = require("mssql");
+var mysql = require("mysql");
 var app = express();
+var bcrypt = require("bcrypt");
 
 // Setting Base directory
 app.use(bodyParser.json());
@@ -23,16 +24,32 @@ app.use(function (req, res, next) {
  });
 
 //Initiallising connection string
-var dbConfig = {
+/*var dbConfig = {
     user:  "developer",
     password: "developer",
-    server: "automaticdoorctrldev.cap2cfj45nxa.us-east-1.rds.amazonaws.com",
+    server: "192.168.56.103",
     database: "users"
-};
+};*/
+var connection = mysql.createConnection({
+	user:		"developer",
+	password:	"developer",
+	host:		"192.168.56.103",
+	database:	"users"
+});
 
 //Function to connect to database and execute query
 var  executeQuery = function(res, query){	
-	sql.connect(dbConfig, function (err) {
+	connection.query(query, function (error, results, fields) {
+		if (error) {
+			console.log("Database error :- " + error);
+			res.send(error);
+		}
+		else {
+			console.log(results);
+			res.send(results);
+		}
+	});
+	/*sql.connect(dbConfig, function (err) {
 		if (err) {   
 			console.log("Error while connecting database :- " + err);
 			res.send(err);
@@ -52,46 +69,77 @@ var  executeQuery = function(res, query){
 				}
 			});
 		}
-	});	
+	});*/
 }
 
 // get active student data **
 app.get("/api/active_user", function(req , res){
-	var query = "select * from [students] where TRIM(LOWER(Status)) = 'active' ";
+	var query = "select * from students where Status = 'Active' ";
 	executeQuery (res, query);
 });
 
 // get student request data **
 app.get("/api/inactive_user", function(req , res){
-	var query = "select * from [students] where TRIM(LOWER(Status)) = 'request' ";
+	var query = "select * from students where Status = 'Request' ";
 	executeQuery (res, query);
 });
 
 //add all student requests to active**
 app.get("/api/addAll", function(req , res){
-	var query = "UPDATE [students] SET Status = 'Active' WHERE TRIM(LOWER(Status)) = 'request'";
+	var query = "UPDATE students SET Status = 'Active' WHERE Status = 'Request'";
 	executeQuery (res, query);
 });
 
 // login to the app  for students**
 app.post("/api/login", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "select * from [students] where RCSid = '" + req.body.RCSid+"'" ;
+	var query = "select * from `students` where `RCSid` = '" + req.body.RCSid +"'" ;
 	console.log(query);
 	executeQuery (res, query);
 });
+
 // login to the app  for admin**
 app.post("/api/admin/login", function(req , res){
 	console.log(req.body.username);
-	var query = "select * from [admin] where username = '" + req.body.username+"' and password = '" + req.body.password+"'" ;
+	var query = "select * from admin where username = '" + req.body.username + "'";
 	console.log(query);
-	executeQuery (res, query);
+	connection.query(query, function (connError, results, fields) {
+		if (connError) {
+			console.log("Database error :- " + connError);
+			res.send(connError);
+		}
+		else {
+			if(results.length > 0) {
+				bcrypt.compare(req.body.password, results[0].password, function(bcryptError, hashMatches) {
+					if (bcryptError) {
+						console.log("bcrypt error :- " + bcryptError);
+						res.send(bcryptError);
+					}
+					else {
+						if(hashMatches) {
+							console.log(results);
+							res.send(results);
+						}
+						else {
+							console.log("Wrong password!");
+							res.send([]);
+						}
+					}
+
+				});
+			}
+			else {
+				console.log("No matching users found");
+				res.send([]);
+			}
+		}
+	});
 });
 
 //register for the app ** 
 app.post("/api/request-access", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "INSERT INTO [students] (RCSid,Status) VALUES ('"+req.body.RCSid+"', 'Request')";
+	var query = "INSERT INTO students (RCSid,Status) VALUES ('"+req.body.RCSid+"', 'Request')";
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -99,7 +147,7 @@ app.post("/api/request-access", function(req , res){
 //add singular student to active status **
 app.post("/api/addtoActive", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "UPDATE [students] SET Status = 'Active' WHERE RCSid = '" + req.body.RCSid+"'";
+	var query = "UPDATE students SET Status = 'Active' WHERE RCSid = '" + req.body.RCSid+"'";
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -107,7 +155,7 @@ app.post("/api/addtoActive", function(req , res){
 //remove a student from active access **
 app.post("/api/remove", function(req , res){
 	console.log(req.body.RCSid);
-	var query = "DELETE FROM [students] WHERE RCSid = '" + req.body.RCSid+"'";
+	var query = "DELETE FROM students WHERE RCSid = '" + req.body.RCSid+"'";
 	console.log(query);
 	executeQuery (res, query);
 });
@@ -115,13 +163,14 @@ app.post("/api/remove", function(req , res){
 //submit a location complaint **
 app.post("/api/submit-complaint", function(req , res){
 	console.log(req.body.location);
-	var query = "INSERT INTO [complaints] (location,message) VALUES ('"+req.body.Location+"', '"+req.body.Message+"')";
+	var query = "INSERT INTO complaints (location,message) VALUES ('"+req.body.Location+"', '"+req.body.Message+"')";
 	console.log(query);
 	executeQuery (res, query);
 });
 
 // list complaints made
 app.get("/api/get-complaints", function(req , res){
-	var query = "select * from [complaints] ";
+	var query = "select * from complaints ";
 	executeQuery (res, query);
 });
+
